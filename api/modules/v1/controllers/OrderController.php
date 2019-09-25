40c8b9df2b1c9af039cb02ec5499aa6104625b51
Denis Kochanovskiy <dkochanovskiy@gmail.com>
2019-09-25 10:03:04 +0300

    receiving token

--- api/modules/v1/controllers/OrderController.php
+++ api/modules/v1/controllers/OrderController.php
@@ -2,155 +2,89 @@
 
 namespace api\modules\v1\controllers;
 
+use common\models\Auth;
+use \yii\rest\Controller;
+use api\modules\v1\models\Order;
+use api\modules\v1\components\Pagination;
 use api\modules\v1\forms\{
     OrderCreateForm,
     OrderStatusChangeForm
 };
-use api\modules\v1\models\Order;
-use common\models\Auth;
-use common\models\User;
-use yii\filters\AccessControl;
-use yii\rest\Controller;
-use api\modules\v1\components\Pagination;
 
 class OrderController extends Controller
 {
     public $modelClass = Order::class;
 
-    public function behaviors()
-    {
-        $behaviors = parent::behaviors();
-        $behaviors['access'] = [
-            'class' => AccessControl::class,
-            'only' => ['login', 'logout', 'signup'],
-            'rules' => [
-                [
-                    'allow' => true,
-                    'actions' => ['login', 'signup'],
-                    'roles' => ['?'],
-                ],
-                [
-                    'allow' => true,
-                    'actions' => ['logout'],
-                    'roles' => ['@'],
-                ],
-            ],
-        ];
 
-        return $behaviors;
+    public function getToken ()
+    {
+        $token = \Yii::$app->request->headers->get('token');
+
+        if (!$token)
+        {
+            return false;
+        }
+
+        return $token;
     }
 
     public function actionIndex()
     {
-        $login = \Yii::$app->request->authUser;
-        $auth_key= \Yii::$app->request->authPassword;
-
-        $token = \Yii::$app->request->get('token');
-
-        $user = User::find()
-            ->andWhere(['login' => $login])
-            ->andWhere(['auth_key' => $auth_key])
-            ->one();
-
-        $auth = Auth::find()
-            ->andWhere(['token' => $token])
-            ->one();
-
-        if ($user) {
-            if($auth) {
-                $query = Order::find();
-                $limit = \Yii::$app->request->get('limit');
-                $offset = \Yii::$app->request->get('offset');
-
-                $pagination = new Pagination([
-                    'totalCount' => $query->count(),
-                ]);
-
-                $order['data'] = $query
-                    ->offset($offset)
-                    ->limit($limit)
-                    ->all();
-
-                $order['recordsTotal'] = $pagination->totalCount;
-                $order['recordsFiltered'] = $pagination->totalCount;
-
-                return $order;
-            } else {
-                return 'Токен отсутствует или введен не верно';
-            }
-        } else {
-            return 'Логин или пароль не верный';
+        if (!$this->getToken()) {
+            return 'Токен не введен';
         }
+
+        $query = Order::find();
+        $limit = \Yii::$app->request->get('limit');
+        $offset = \Yii::$app->request->get('offset');
+
+        $pagination = new Pagination([
+            'totalCount' => $query->count(),
+        ]);
+
+        $order['data'] = $query
+            ->offset($offset)
+            ->limit($limit)
+            ->all();
+
+        $order['recordsTotal'] = $pagination->totalCount;
+        $order['recordsFiltered'] = $pagination->totalCount;
+
+        return $order;
     }
 
     public function actionCreate()
     {
-        $login = \Yii::$app->request->authUser;
-        $auth_key= \Yii::$app->request->authPassword;
+        if (!$this->getToken()) {
+            return 'Токен не введен';
+        }
 
-        $token = \Yii::$app->request->get('token');
-
-        $user = User::find()
-            ->andWhere(['login' => $login])
-            ->andWhere(['auth_key' => $auth_key])
-            ->one();
-
-        $auth = Auth::find()
-            ->andWhere(['token' => $token])
-            ->one();
-
-        if ($user) {
-            if ($auth) {
-                $form = new OrderCreateForm();
-                $form->load(\Yii::$app->request->post(), '');
-                if ($form->validate()) {
-                    $order = Order::createNewOrder($form);
-                    return $order;
-                } else {
-                    return $form->errors;
-                }
-            } else {
-                return 'Токен отсутствует или введен не верно';
-            }
+        $form = new OrderCreateForm();
+        $form->load(\Yii::$app->request->post(), '');
+        if ($form->validate()) {
+            $order = Order::createNewOrder($form);
+            return $order;
         } else {
-            return 'Логин или пароль не верный';
+            return $form->errors;
         }
     }
 
     public function actionUpdate()
     {
-        $login = \Yii::$app->request->authUser;
-        $auth_key= \Yii::$app->request->authPassword;
+        if (!$this->getToken()) {
+            return 'Токен не введен';
+        }
 
-        $token = \Yii::$app->request->get('token');
-
-        $user = User::find()
-            ->andWhere(['login' => $login])
-            ->andWhere(['auth_key' => $auth_key])
-            ->one();
-
-        $auth = Auth::find()
-            ->andWhere(['token' => $token])
-            ->one();
-
-        if ($user) {
-            if ($auth) {
-                $form = new OrderStatusChangeForm();
-                $form->load(\Yii::$app->request->post(), '');
-                if ($form->validate()) {
-                    $order = Order::findOne(['id' => $form->id]);
-                    $order->updateAttributes([
-                        'status_id' => $form->status_id,
-                    ]);
-                    return $order;
-                } else {
-                    return $form->errors;
-                }
-            } else {
-                return 'Токен отсутствует или введен не верно';
-            }
+        $form = new OrderStatusChangeForm();
+        $form->load(\Yii::$app->request->post(), '');
+        if ($form->validate()) {
+            $order = Order::findOne(['id' => $form->id]);
+            $order->updateAttributes([
+                'status_id' => $form->status_id,
+            ]);
+            return $order;
         } else {
-            return 'Логин или пароль не верный';
+            return $form->errors;
         }
     }
 }
